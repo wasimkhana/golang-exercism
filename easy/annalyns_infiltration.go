38d93d1207a9d6fb4bd141f67e68c6a0d0d44952
Wasim Khan <wasimkhan042>
2021-09-14 12:43:48 +0500

    added solution to annalyns_infiltration problem (category:easy)

---
+++ easy/annalyns_infiltration.go
@@ -0,0 +1,38 @@
+package annalyn
+
+// CanFastAttack can be executed only when the knight is sleeping
+func CanFastAttack(knightIsAwake bool) bool {
+	if !knightIsAwake {
+        return true
+    } 
+    return false
+}
+
+// CanSpy can be executed if at least one of the characters is awake
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+        return true
+    }
+    return false
+}
+
+// CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	if prisonerIsAwake && !(archerIsAwake) {
+        return true
+    }
+    return false
+}
+
+// CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
+// or if Annalyn's pet dog is with her and the archer is sleeping
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	if prisonerIsAwake && (knightIsAwake ==false && archerIsAwake==false) {
+        return true
+    } else if petDogIsPresent && !(archerIsAwake) {
+        return true
+    } else {
+    return false
+    }
+}
+
